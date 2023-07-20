66dee3b8e6408f15d3bca5f3cd5175baa7a3f455
BoggavarapuMahesh <136325414+BoggavarapuMahesh@users.noreply.github.com>
2023-07-20 11:22:00 +0530

    Update newcircuit.ts

--- tasks/newcircuit.ts
+++ tasks/newcircuit.ts
@@ -1,54 +1,57 @@
 import { ethers } from "hardhat";
 import { task } from "hardhat/config";
 import { CircomCircuitConfig } from "hardhat-circom";
-const fs = require("fs");
+import fs from "fs";
 
-const CONFIG_PATH = process.env.BASE_PATH + "/circuits.config.json";
-const DIR_PATH = process.env.BASE_PATH + "/circuits/";
+const CONFIG_PATH = `${process.env.BASE_PATH}/circuits.config.json`;
+const DIR_PATH = `${process.env.BASE_PATH}/circuits/`;
 
 task("newcircuit", "Generate config for a new circuit")
   .addParam("name", "Name of the circuit")
-  .setAction(async (taskArgs, {}) => {
+  .setAction(async (taskArgs, hre) => {
+    const circuitsConfig: CircomCircuitConfig[] = [];
 
-    let circuitsConfig: CircomCircuitConfig[] = [];
-
-    // check if file exists
+    // Check if the configuration file exists, read it if present
     if (fs.existsSync(CONFIG_PATH)) {
-      circuitsConfig = JSON.parse(fs.readFileSync(CONFIG_PATH, "utf8"));
+      const configContent = fs.readFileSync(CONFIG_PATH, "utf8");
+      if (configContent) {
+        const existingConfigs = JSON.parse(configContent);
+        if (Array.isArray(existingConfigs)) {
+          circuitsConfig.push(...existingConfigs);
+        }
+      }
     }
 
-    // try creating a directory
+    // Create a new circuit directory and files
     try {
-      if (!fs.existsSync(DIR_PATH + taskArgs.name)) {
-        // create file
-        fs.mkdirSync(DIR_PATH + taskArgs.name);
+      const circuitDirPath = `${DIR_PATH}${taskArgs.name}`;
+      if (!fs.existsSync(circuitDirPath)) {
+        fs.mkdirSync(circuitDirPath);
+        fs.writeFileSync(`${circuitDirPath}/input.json`, "{}");
+        fs.writeFileSync(`${circuitDirPath}/circuit.circom`, "");
       }
     } catch (err) {
-      console.error(err);
+      console.error("Failed to create the circuit directory:", err);
+      return;
     }
 
-    // create input and circuit files
-    fs.closeSync(fs.openSync(DIR_PATH + taskArgs.name + "/input.json", 'w'))
-    fs.closeSync(fs.openSync(DIR_PATH + taskArgs.name + "/circuit.circom", 'w'))
-
-    // create a new circuit config
+    // Create a new circuit configuration
     const circuitConfig: CircomCircuitConfig = {
       name: taskArgs.name,
       version: 2,
       protocol: "groth16",
-      circuit: taskArgs.name + "/circuit.circom",
-      input: taskArgs.name + "/input.json",
-      wasm: taskArgs.name + "/out/circuit.wasm",
-      zkey: taskArgs.name + "/out/" + taskArgs.name + ".zkey",
-      vkey: taskArgs.name + "/out/" + taskArgs.name + ".vkey",
-      r1cs: taskArgs.name + "/out/" + taskArgs.name + ".r1cs",
-      // Used when specifying `--deterministic` instead of the default of all 0s
+      circuit: `${taskArgs.name}/circuit.circom`,
+      input: `${taskArgs.name}/input.json`,
+      wasm: `${taskArgs.name}/out/circuit.wasm`,
+      zkey: `${taskArgs.name}/out/${taskArgs.name}.zkey`,
+      vkey: `${taskArgs.name}/out/${taskArgs.name}.vkey`,
+      r1cs: `${taskArgs.name}/out/${taskArgs.name}.r1cs`,
       beacon: "0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f",
     };
 
-    // add circuit config to array
+    // Add the new circuit config to the array
     circuitsConfig.push(circuitConfig);
 
-    // write to file
+    // Write the updated configurations to the file
     fs.writeFileSync(CONFIG_PATH, JSON.stringify(circuitsConfig, null, 2));
 });
