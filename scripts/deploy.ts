73bb3dae0dbadd88b5b1eba54f0f5da9ee0f6742
BoggavarapuMahesh <136325414+BoggavarapuMahesh@users.noreply.github.com>
2023-07-20 11:23:41 +0530

    Update deploy.ts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,88 +1,68 @@
 import { ethers } from "hardhat";
-import { utils } from "ffjavascript";
-import { BigNumber, BigNumberish } from "ethers";
+import { BigNumber } from "ethers";
 
-const { unstringifyBigInts } = utils;
 const fs = require("fs");
 const snarkjs = require("snarkjs");
 
 interface ICallData {
-  pi_a: BigNumberish[];
-  pi_b: BigNumberish[][];
-  pi_c: BigNumberish[];
-  input: BigNumberish[];
+  pi_a: BigNumber[];
+  pi_b: BigNumber[][];
+  pi_c: BigNumber[];
+  input: BigNumber[];
 }
 
-const BASE_PATH = "./circuits/CustomCircuit/";
+const CIRCUIT_PATH = "./circuits/MyCustomCircuit/";
 
-function p256(n: any): BigNumber {
-  let nstr = n.toString(16);
-  while (nstr.length < 64) nstr = "0" + nstr;
-  nstr = `0x${nstr}`;
-  return BigNumber.from(nstr);
+function formatBigNumberArray(arr: BigNumberish[]): BigNumber[] {
+  return arr.map((n) => BigNumber.from(n.toString()));
 }
 
 async function generateCallData(): Promise<ICallData> {
-  let zkProof = await generateProof();
+  const zkProof = await generateProof();
 
-  const proof = unstringifyBigInts(zkProof.proof);
-  const pub = unstringifyBigInts(zkProof.publicSignals);
+  const proof = zkProof.proof;
+  const publicSignals = zkProof.publicSignals;
 
-  let inputs = "";
-  for (let i = 0; i < pub.length; i++) {
-    if (inputs != "") inputs = inputs + ",";
-    inputs = inputs + p256(pub[i]);
-  }
-
-  let pi_a = [p256(proof.pi_a[0]), p256(proof.pi_a[1])]
-  let pi_b = [[p256(proof.pi_b[0][1]), p256(proof.pi_b[0][0])], [p256(proof.pi_b[1][1]), p256(proof.pi_b[1][0])]]
-  let pi_c = [p256(proof.pi_c[0]), p256(proof.pi_c[1])]
-  let input = [inputs]
+  const pi_a = formatBigNumberArray(proof.pi_a);
+  const pi_b = proof.pi_b.map((row: BigNumberish[]) => formatBigNumberArray(row));
+  const pi_c = formatBigNumberArray(proof.pi_c);
+  const input = formatBigNumberArray(publicSignals);
 
   return { pi_a, pi_b, pi_c, input };
 }
 
 async function generateProof() {
-
-  // read input parameters
-  const inputData = fs.readFileSync(BASE_PATH + "input.json", "utf8");
+  const inputData = fs.readFileSync(CIRCUIT_PATH + "input.json", "utf8");
   const input = JSON.parse(inputData);
 
-  // calculate witness
   const out = await snarkjs.wtns.calculate(
     input,
-    BASE_PATH + "out/circuit.wasm",
-    BASE_PATH + "out/circuit.wtns"
-  )
+    CIRCUIT_PATH + "out/circuit.wasm",
+    CIRCUIT_PATH + "out/circuit.wtns"
+  );
 
-  // calculate proof
   const proof = await snarkjs.groth16.prove(
-    BASE_PATH + "out/CustomCircuit.zkey",
-    BASE_PATH + "out/circuit.wtns"
-  )
+    CIRCUIT_PATH + "out/MyCustomCircuit.zkey",
+    CIRCUIT_PATH + "out/circuit.wtns"
+  );
 
-  // write proof to file
-  fs.writeFileSync(BASE_PATH + "out/proof.json", JSON.stringify(proof, null, 1));
+  fs.writeFileSync(CIRCUIT_PATH + "out/proof.json", JSON.stringify(proof, null, 1));
 
-  return proof
+  return proof;
 }
 
 async function main() {
-  // deploy contract
-  const Verifier = await ethers.getContractFactory("./contracts/CustomCircuitVerifier.sol:Verifier");
+  const Verifier = await ethers.getContractFactory("MyCustomCircuitVerifier");
   const verifier = await Verifier.deploy();
   await verifier.deployed();
 
   console.log(`Verifier deployed to ${verifier.address}`);
 
-  // generate proof call data
-  const {pi_a, pi_b, pi_c, input} = await generateCallData();
+  const { pi_a, pi_b, pi_c, input } = await generateCallData();
 
-  // verify proof on contract
-  //@ts-ignore
-  const tx = await verifier.verifyProof(pi_a, pi_b, pi_c, input)
-  
-  console.log(`Verifier result: ${tx}`)
+  const tx = await verifier.verifyProof(pi_a, pi_b, pi_c, input);
+
+  console.log(`Verifier result: ${tx}`);
   console.assert(tx == true, "Proof verification failed!");
 
   process.exit(0);
