001fe417c60ac2a07437fc03b6f37aa37ca0604e
BoggavarapuMahesh <136325414+BoggavarapuMahesh@users.noreply.github.com>
2023-07-20 11:18:29 +0530

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,17 +1,14 @@
+
+
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-// https://github.com/projectsophon/hardhat-circom
 import "hardhat-circom";
-// circuits
-import circuits = require('./circuits.config.json');
-import 'dotenv/config'
+import circuitsConfig from './circuits.config.json';
+import 'dotenv/config';
 
-
-// set env var to the root of the project
 process.env.BASE_PATH = __dirname;
 
-// tasks
-import "./tasks/newcircuit.ts"
+import "./tasks/newcircuit.ts";
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -24,18 +21,20 @@ const config: HardhatUserConfig = {
       }
     ]
   },
-  sepolia: {
-    url: `https://rpc.ankr.com/polygon_mumbai`,
-    accounts: [process.env.MUMBAIPRIVATEKEY]
+  networks:{
+    mumbai: {
+      url: `https://rpc.ankr.com/polygon_mumbai`,
+      accounts: [process.env.MUMBAIPRIVATEKEY]
+    }
   },
-    circom: {
-    // (optional) Base path for input files, defaults to `./circuits/`
+  
+  
+  circom: {
     inputBasePath: "./circuits",
-    // (required) The final ptau file, relative to inputBasePath, from a Phase 1 ceremony
     ptau: "powersOfTau28_hez_final_12.ptau",
-    // (required) Each object in this array refers to a separate circuit
-    circuits: JSON.parse(JSON.stringify(circuits))
+    circuits: JSON.parse(JSON.stringify(circuitsConfig))
   },
 };
 
 export default config;
+
